52977eb1bb3a34b934135dffbd232a1e5455ba9e
John Russel M. Libarnes <johnrussellibarnes2@gmail.com>
2025-10-27 09:55:46 +0800

    Added UserInputs and Logo

--- HorizonBookingSystem/Form1.Designer.cs
+++ HorizonBookingSystem/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace HorizonBookingSystem
 {
-    partial class Form1
+    partial class Loginpage
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Loginpage));
+            this.Usertxt = new System.Windows.Forms.TextBox();
+            this.UserLbl = new System.Windows.Forms.Label();
+            this.PassLbl = new System.Windows.Forms.Label();
+            this.Passtxt = new System.Windows.Forms.TextBox();
+            this.Loginbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Usertxt
+            // 
+            this.Usertxt.Location = new System.Drawing.Point(98, 151);
+            this.Usertxt.Name = "Usertxt";
+            this.Usertxt.Size = new System.Drawing.Size(100, 20);
+            this.Usertxt.TabIndex = 0;
+            // 
+            // UserLbl
+            // 
+            this.UserLbl.AutoSize = true;
+            this.UserLbl.Location = new System.Drawing.Point(23, 154);
+            this.UserLbl.Name = "UserLbl";
+            this.UserLbl.Size = new System.Drawing.Size(58, 13);
+            this.UserLbl.TabIndex = 1;
+            this.UserLbl.Text = "Username:";
+            // 
+            // PassLbl
+            // 
+            this.PassLbl.AutoSize = true;
+            this.PassLbl.Location = new System.Drawing.Point(25, 189);
+            this.PassLbl.Name = "PassLbl";
+            this.PassLbl.Size = new System.Drawing.Size(56, 13);
+            this.PassLbl.TabIndex = 2;
+            this.PassLbl.Text = "Password:";
+            // 
+            // Passtxt
+            // 
+            this.Passtxt.Location = new System.Drawing.Point(98, 182);
+            this.Passtxt.Name = "Passtxt";
+            this.Passtxt.Size = new System.Drawing.Size(100, 20);
+            this.Passtxt.TabIndex = 3;
+            // 
+            // Loginbtn
+            // 
+            this.Loginbtn.Location = new System.Drawing.Point(109, 218);
+            this.Loginbtn.Name = "Loginbtn";
+            this.Loginbtn.Size = new System.Drawing.Size(75, 23);
+            this.Loginbtn.TabIndex = 4;
+            this.Loginbtn.Text = "Login";
+            this.Loginbtn.UseVisualStyleBackColor = true;
+            // 
+            // Loginpage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.Loginbtn);
+            this.Controls.Add(this.Passtxt);
+            this.Controls.Add(this.PassLbl);
+            this.Controls.Add(this.UserLbl);
+            this.Controls.Add(this.Usertxt);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Loginpage";
+            this.Text = "Horizon";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox Usertxt;
+        private System.Windows.Forms.Label UserLbl;
+        private System.Windows.Forms.Label PassLbl;
+        private System.Windows.Forms.TextBox Passtxt;
+        private System.Windows.Forms.Button Loginbtn;
     }
 }
 
--- HorizonBookingSystem/Form1.cs
+++ HorizonBookingSystem/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace HorizonBookingSystem
 {
-    public partial class Form1 : Form
+    public partial class Loginpage : Form
     {
-        public Form1()
+        public Loginpage()
         {
             InitializeComponent();
         }
