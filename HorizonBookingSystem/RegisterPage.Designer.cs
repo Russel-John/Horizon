89f29a9dbd589781f86c557d5d3368264f8e13b6
John Russel M. Libarnes <johnrussellibarnes2@gmail.com>
2025-11-18 17:58:59 +0800

    Registration Form Functionalities Working

--- HorizonBookingSystem/RegisterPage.Designer.cs
+++ HorizonBookingSystem/RegisterPage.Designer.cs
@@ -28,12 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegisterPage));
+            this.txtBoxPassword = new System.Windows.Forms.TextBox();
+            this.PassLbl = new System.Windows.Forms.Label();
+            this.UserLbl = new System.Windows.Forms.Label();
+            this.txtBoxUsername = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtBoxEmail = new System.Windows.Forms.TextBox();
+            this.Registerbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBoxPassword
+            // 
+            this.txtBoxPassword.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxPassword.ForeColor = System.Drawing.Color.DarkBlue;
+            this.txtBoxPassword.Location = new System.Drawing.Point(257, 222);
+            this.txtBoxPassword.Name = "txtBoxPassword";
+            this.txtBoxPassword.PasswordChar = '●';
+            this.txtBoxPassword.Size = new System.Drawing.Size(269, 23);
+            this.txtBoxPassword.TabIndex = 7;
+            this.txtBoxPassword.TextChanged += new System.EventHandler(this.txtBoxPassword_TextChanged);
+            // 
+            // PassLbl
+            // 
+            this.PassLbl.AutoSize = true;
+            this.PassLbl.BackColor = System.Drawing.Color.Transparent;
+            this.PassLbl.Font = new System.Drawing.Font("Cascadia Code", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PassLbl.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.PassLbl.Location = new System.Drawing.Point(253, 199);
+            this.PassLbl.Name = "PassLbl";
+            this.PassLbl.Size = new System.Drawing.Size(81, 20);
+            this.PassLbl.TabIndex = 6;
+            this.PassLbl.Text = "Password";
+            // 
+            // UserLbl
+            // 
+            this.UserLbl.AutoSize = true;
+            this.UserLbl.BackColor = System.Drawing.Color.Transparent;
+            this.UserLbl.Font = new System.Drawing.Font("Cascadia Code", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserLbl.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.UserLbl.Location = new System.Drawing.Point(253, 140);
+            this.UserLbl.Name = "UserLbl";
+            this.UserLbl.Size = new System.Drawing.Size(81, 20);
+            this.UserLbl.TabIndex = 5;
+            this.UserLbl.Text = "Username";
+            // 
+            // txtBoxUsername
+            // 
+            this.txtBoxUsername.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxUsername.ForeColor = System.Drawing.Color.DarkBlue;
+            this.txtBoxUsername.Location = new System.Drawing.Point(257, 163);
+            this.txtBoxUsername.Name = "txtBoxUsername";
+            this.txtBoxUsername.Size = new System.Drawing.Size(269, 23);
+            this.txtBoxUsername.TabIndex = 4;
+            this.txtBoxUsername.TextChanged += new System.EventHandler(this.txtBoxUsername_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Cascadia Mono", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label1.Location = new System.Drawing.Point(240, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(306, 39);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Registration Form";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.BackColor = System.Drawing.Color.Transparent;
+            this.lblEmail.Font = new System.Drawing.Font("Cascadia Code", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.lblEmail.Location = new System.Drawing.Point(253, 257);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(54, 20);
+            this.lblEmail.TabIndex = 9;
+            this.lblEmail.Text = "Email";
+            // 
+            // txtBoxEmail
+            // 
+            this.txtBoxEmail.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxEmail.ForeColor = System.Drawing.Color.DarkBlue;
+            this.txtBoxEmail.Location = new System.Drawing.Point(257, 289);
+            this.txtBoxEmail.Name = "txtBoxEmail";
+            this.txtBoxEmail.PasswordChar = '●';
+            this.txtBoxEmail.Size = new System.Drawing.Size(269, 23);
+            this.txtBoxEmail.TabIndex = 10;
+            this.txtBoxEmail.TextChanged += new System.EventHandler(this.txtBoxEmail_TextChanged);
+            // 
+            // Registerbtn
+            // 
+            this.Registerbtn.BackColor = System.Drawing.Color.SteelBlue;
+            this.Registerbtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Registerbtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Registerbtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Registerbtn.Font = new System.Drawing.Font("Cascadia Mono", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Registerbtn.ForeColor = System.Drawing.Color.White;
+            this.Registerbtn.Location = new System.Drawing.Point(257, 333);
+            this.Registerbtn.Name = "Registerbtn";
+            this.Registerbtn.Size = new System.Drawing.Size(269, 38);
+            this.Registerbtn.TabIndex = 11;
+            this.Registerbtn.Text = "Register";
+            this.Registerbtn.UseVisualStyleBackColor = false;
+            this.Registerbtn.Click += new System.EventHandler(this.Registerbtn_Click);
+            // 
+            // RegisterPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Registerbtn);
+            this.Controls.Add(this.txtBoxEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxPassword);
+            this.Controls.Add(this.PassLbl);
+            this.Controls.Add(this.UserLbl);
+            this.Controls.Add(this.txtBoxUsername);
+            this.DoubleBuffered = true;
+            this.Name = "RegisterPage";
             this.Text = "RegisterPage";
+            this.Load += new System.EventHandler(this.RegisterPage_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtBoxPassword;
+        private System.Windows.Forms.Label PassLbl;
+        private System.Windows.Forms.Label UserLbl;
+        private System.Windows.Forms.TextBox txtBoxUsername;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtBoxEmail;
+        private System.Windows.Forms.Button Registerbtn;
     }
 }
--- HorizonBookingSystem/RegisterPage.cs
+++ HorizonBookingSystem/RegisterPage.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,47 @@ namespace HorizonBookingSystem
 {
     public partial class RegisterPage : Form
     {
+        private BookingDBEntities db = new BookingDBEntities();
         public RegisterPage()
         {
             InitializeComponent();
+        }
+
+        private void txtBoxUsername_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtBoxPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtBoxEmail_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Registerbtn_Click(object sender, EventArgs e)
+        {
+            Users newUser = new Users
+            {
+                username = txtBoxUsername.Text,
+                password = txtBoxPassword.Text, 
+                email = txtBoxEmail.Text,
+                roleID = 1 
+            }; 
+            db.Users.Add(newUser);
+            db.SaveChanges();
+            MessageBox.Show("Registration Successful", "Registered", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Loginpage logpage = new Loginpage();
+            logpage.Show();
+            this.Hide();
+        }
+
+        private void RegisterPage_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
